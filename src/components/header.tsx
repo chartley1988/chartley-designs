7c398d13df102173dcfab9a7b5bcf21fd5d5af52
Carson Hartley <chartley1988@gmail.com>
2023-03-04 14:34:59 -0700

    Added logo to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,13 @@
 import SocialBar from './social_bar';
 import styles from '../styles/header.module.css';
 import NavMenu from './nav_menu';
+import Image from 'next/image';
 
 function Header() {
 	return (
 		<div className={styles.container}>
 			<div className={styles.title}>
+				<Image src="icons/chart_logo.svg" width={48} height={48} />
 				<h1>Chartley Designs</h1>
 				<div className={styles.social}>
 					<SocialBar />
