d6cf89a69d86af84f27a04d71c2f308ed9594b17
Carson Hartley <chartley1988@gmail.com>
2023-03-04 14:39:34 -0700

    alt fix for website logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ function Header() {
 	return (
 		<div className={styles.container}>
 			<div className={styles.title}>
-				<Image src="icons/chart_logo.svg" width={48} height={48} />
+				<Image src="icons/chart_logo.svg" width={48} height={48} alt="Website Logo" />
 				<h1>Chartley Designs</h1>
 				<div className={styles.social}>
 					<SocialBar />
